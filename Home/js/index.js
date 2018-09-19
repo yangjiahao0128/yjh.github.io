3467a4d7a64f7643ac14c00417dd30df39c40728
厉害的杨嘉浩 <35673014+yangjiahao0128@users.noreply.github.com>
2018-09-20 07:50:46 +0800

    xinagmu

---
+++ Home/js/index.js
@@ -0,0 +1,35 @@
+(function(){
+class action{
+	constructor(){
+		this.skip();
+		this.photo();
+	}
+	skip(){
+		$("li").on("click",function(){
+			var str = $(this).index();
+			$("li a").removeClass("active");
+			$(this.children).addClass("active");
+			let num = document.body.clientWidth;
+			$(".slides").css("transform","translateX(-"+str*num+"px)")
+		})
+	}
+	photo(){
+		$(".thumb").on("click",function(){
+			$(".BigBox").removeClass("xiao")
+			var str = $(this)[0]["childNodes"][3].children[0]["attributes"][0]["nodeValue"];
+			$(".BigBox img")[0].src = str
+		});
+		$(".close").on("click",function(){
+			if($(".BigBox").css("display")=="block"){
+				$(".BigBox").addClass("xiao")
+			}
+		})
+		$(".BigBox").on("click",function(){
+			if($(".BigBox").css("display")=="block"){
+				$(".BigBox").addClass("xiao")
+			}
+		})
+	}
+}
+new action();
+}())
